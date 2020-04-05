518bbe2150e204ceeb12e9dac49e64467bbc1272
Nicholas Keller <nic.keller328@gmail.com>
2020-04-05 17:36:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  attr_writer   :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    
+  end
+  
+  
+  
+  
+  
+  
+end
