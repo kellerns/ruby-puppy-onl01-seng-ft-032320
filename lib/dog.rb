1054bad6f4226ae21f7af218f4f2a303d4e22252
Nicholas Keller <nic.keller328@gmail.com>
2020-04-05 17:38:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,11 +6,18 @@ class Dog
   
   def initialize(name)
     @name = name
-    
+    @@all << self
   end
   
+  def all
+    @@all
+  end
   
-  
+  def print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
   
   
   
